dcc7a6fc9a496172c339da1165acb3aca13a4169
AlexMachariaMaranga <amaranga22@gmail.com>
2023-03-21 22:18:18 +0300

    commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+    # def show
+    #     user = User.find_by(id: session[:user_id])
+    # if user
+    #   render json: user
+    # else
+    #   render json: { error: "Not authorized" }, status: :unauthorized
+    # end
+    # end
+    def show
+        user = User.find(session[:user_id])
+        render json: user
+      end
+end
